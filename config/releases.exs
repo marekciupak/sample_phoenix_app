6e59379670cd01d8ed38ee252b8e047764d6060c
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2020-04-07 23:05:50 +0200

    Format code

--- config/releases.exs
+++ config/releases.exs
@@ -23,7 +23,8 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
-config :my_app, MyAppWeb.Endpoint, server: true,
+config :my_app, MyAppWeb.Endpoint,
+  server: true,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
